adc2fff5ae69961ed4a88c94ae257554f2b5534d
Jessica Natovich <jessicanatovich@yahoo.com>
2019-11-26 22:10:48 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,6 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  meal
+  return meal # return here is optional. Ruby methods automatically return the last expression
+end
